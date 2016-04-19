475e7f4bb1393e78da2124ddd9c9a31b1eb94d17
Bob Holt <bobholt@gmail.com>
2016-04-19 07:46:06 -0400

    add d3 and update webpack config

--- d3-react/webpack.config.js
+++ d3-react/webpack.config.js
@@ -1,7 +1,11 @@
 var getConfig = require('hjs-webpack');
 
-module.exports = getConfig({
+var config = getConfig({
   in: 'src/index.jsx',
   out: 'public',
   clearBeforeBuild: true,
 });
+
+config.module.loaders.push({ test: /\.csv$/, loader: 'file-loader' });
+
+module.exports = config;
